7e4e7949ebfa7c6e597f22dbf643485789d66dde
devensiv <devensiv@gmail.com>
2022-07-20 20:25:47 +0200

    receiving of rhiz_message

--- src/messages/rhiz_message.rs
+++ src/messages/rhiz_message.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
-use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader};
 use tokio_rustls::server::TlsStream;
 
 use crate::User;
 
-use std::io;
+use std::io::{self, ErrorKind};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub enum RhizMessage {
@@ -13,6 +13,7 @@ pub enum RhizMessage {
     WantsRoom(User),
     AcceptedRoom(Option<[u8; 32]>),
     ServerError(String),
+    Shutdown(),
 }
 
 impl RhizMessage {
@@ -21,12 +22,27 @@ impl RhizMessage {
         T: AsyncRead + AsyncWrite + Unpin,
     {
         // use 192 byte buffer since we restrict wants room string to max 128 characters
-        let bytes = postcard::to_vec_cobs::<Self, 64>(&self)
-            .expect("there was a big oopsie during developement!
-            serialization size for RhizMessage was bigger then the specified buffer size");
+        let bytes = postcard::to_vec_cobs::<Self, 64>(&self).map_err(|_| {
+            io::Error::new(ErrorKind::OutOfMemory, "Unable to serialize RhizMessage, more then 64 byte")
+        })?;
 
         #[cfg(feature = "debug")]
         println!("sent msg");
         tls.write_all(&bytes).await
+    }
+
+    pub async fn recv_with<T>(
+        tls: &mut BufReader<TlsStream<T>>,
+        buf: &mut Vec<u8>,
+    ) -> io::Result<RhizMessage>
+    where
+        T: AsyncRead + AsyncWrite + Unpin,
+    {
+        // 0 means EOF so we shutdown the connection
+        if 0 == tls.read_until(0, buf).await? {
+            return Ok(RhizMessage::Shutdown());
+        }
+
+        postcard::from_bytes_cobs(buf).map_err(|err| io::Error::new(io::ErrorKind::Other, err))
     }
 }
