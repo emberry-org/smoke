f1fc35b31a7d0b3cb77c317f14f704c19d439736
devensiv <devensiv@gmail.com>
2023-02-22 00:20:33 +0100

    test: multi message testcase

--- tests/integration.rs
+++ tests/integration.rs
@@ -35,3 +35,42 @@ async fn stream_test_complex() {
     assert!(signal.is_ok());
     assert_eq!(signal.unwrap(), msg);
 }
+
+#[tokio::test]
+async fn stream_test_multiple() {
+    let msg = Signal::Username("Aurelia".to_string());
+    let msg2 = Signal::Kap;
+    let mut msg_bytes = Vec::<u8>::new();
+    msg2.clone().send_with(&mut msg_bytes).await.unwrap();
+    msg.clone().send_with(&mut msg_bytes).await.unwrap();
+    msg2.clone().send_with(&mut msg_bytes).await.unwrap();
+
+    let stream = Builder::new().read(&msg_bytes).read(&msg_bytes).build();
+    let mut reader = BufReader::new(stream);
+
+    let mut buf = Vec::with_capacity(1024);
+
+    let signal = Signal::recv_with(&mut reader, &mut buf).await;
+    assert!(signal.is_ok());
+    assert_eq!(signal.unwrap(), msg2);
+
+    let signal = Signal::recv_with(&mut reader, &mut buf).await;
+    assert!(signal.is_ok());
+    assert_eq!(signal.unwrap(), msg);
+
+    let signal = Signal::recv_with(&mut reader, &mut buf).await;
+    assert!(signal.is_ok());
+    assert_eq!(signal.unwrap(), msg2);
+
+    let signal = Signal::recv_with(&mut reader, &mut buf).await;
+    assert!(signal.is_ok());
+    assert_eq!(signal.unwrap(), msg2);
+
+    let signal = Signal::recv_with(&mut reader, &mut buf).await;
+    assert!(signal.is_ok());
+    assert_eq!(signal.unwrap(), msg);
+
+    let signal = Signal::recv_with(&mut reader, &mut buf).await;
+    assert!(signal.is_ok());
+    assert_eq!(signal.unwrap(), msg2);
+}
