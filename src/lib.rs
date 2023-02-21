f89acb2d4bb68d66b1e6392b6e6461df61410e9d
devensiv <devensiv@gmail.com>
2023-02-22 00:18:13 +0100

    test: added initial integration test
    added a two test cases for serializing and deserializing signals

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod messages;
 mod user;
-pub mod serial;
 
 pub use messages::signal::Signal;
 pub use user::User;
---
+++ tests/integration.rs
@@ -0,0 +1,37 @@
+use smoke::Signal;
+use tokio::io::BufReader;
+use tokio_test::io::Builder;
+
+#[tokio::test]
+async fn stream_test() {
+    let msg = Signal::Kap;
+    let mut msg_bytes = Vec::<u8>::new();
+    msg.clone().send_with(&mut msg_bytes).await.unwrap();
+
+    let stream = Builder::new().read(&msg_bytes).build();
+    let mut reader = BufReader::new(stream);
+
+    let mut buf = Vec::with_capacity(1024);
+
+    let signal = Signal::recv_with(&mut reader, &mut buf).await;
+
+    assert!(signal.is_ok());
+    assert_eq!(signal.unwrap(), msg);
+}
+
+#[tokio::test]
+async fn stream_test_complex() {
+    let msg = Signal::Username("Aurelia".to_string());
+    let mut msg_bytes = Vec::<u8>::new();
+    msg.clone().send_with(&mut msg_bytes).await.unwrap();
+
+    let stream = Builder::new().read(&msg_bytes).build();
+    let mut reader = BufReader::new(stream);
+
+    let mut buf = Vec::with_capacity(1024);
+
+    let signal = Signal::recv_with(&mut reader, &mut buf).await;
+
+    assert!(signal.is_ok());
+    assert_eq!(signal.unwrap(), msg);
+}
