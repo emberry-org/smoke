587d42b4d6fa2b38e80c5724dad1e5f65448fa86
devensiv <devensiv@gmail.com>
2023-02-25 19:51:15 +0100

    feat: even more benchmarks

--- benches/signal.rs
+++ benches/signal.rs
@@ -1,4 +1,4 @@
-use std::hint::black_box;
+use std::{hint::black_box, io::Cursor};
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use smoke::Signal;
@@ -7,15 +7,42 @@ use tokio_test::io::Builder;
 
 async fn encode() {
     let mut msg_bytes = Vec::<u8>::new();
-    Signal::Username("Aurelia".to_string()).send_with(&mut msg_bytes).await.unwrap();
+    Signal::Username(black_box("Aurelia".to_string()))
+        .send_with(&mut msg_bytes)
+        .await
+        .unwrap();
 }
 
-async fn encode_decode() {
+async fn encode_decode_mock() {
     let mut msg_bytes = Vec::<u8>::new();
-    Signal::Username("Aurelia".to_string()).send_with(&mut msg_bytes).await.unwrap();
+    Signal::Username(black_box("Aurelia".to_string()))
+        .send_with(&mut msg_bytes)
+        .await
+        .unwrap();
 
     let stream = Builder::new().read(&msg_bytes).build();
     let mut reader = BufReader::new(stream);
+    let mut buf = Vec::new();
+
+    let _signal = black_box(Signal::recv_with(&mut reader, &mut buf).await);
+}
+
+async fn encode_decode_cursor() {
+    let mut msg_bytes = Vec::<u8>::new();
+    Signal::Username(black_box("Aurelia".to_string()))
+        .send_with(&mut msg_bytes)
+        .await
+        .unwrap();
+
+    let cursor = Cursor::new(msg_bytes);
+    let mut reader = BufReader::new(cursor);
+    let mut buf = Vec::new();
+
+    let _signal = black_box(Signal::recv_with(&mut reader, &mut buf).await);
+}
+
+async fn decode(bytes: &[u8]) {
+    let mut reader = BufReader::new(bytes);
     let mut buf = Vec::new();
 
     let _signal = black_box(Signal::recv_with(&mut reader, &mut buf).await);
@@ -28,9 +55,20 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("signal");
 
     group.bench_function("encode", |b| b.to_async(&runtime).iter(encode));
-    group.bench_function("encode_decode", |b| {
-        b.to_async(&runtime).iter(encode_decode)
+    group.bench_function("encode_decode_mock", |b| {
+        b.to_async(&runtime).iter(encode_decode_mock)
     });
+    group.bench_function("encode_decode_cursor", |b| {
+        b.to_async(&runtime).iter(encode_decode_cursor)
+    });
+
+    let mut msg_bytes = Vec::<u8>::new();
+    runtime
+        .block_on(Signal::Username("Aurelia".to_string()).send_with(&mut msg_bytes))
+        .unwrap();
+
+    group.bench_with_input("decode", &msg_bytes
+    , |b, i| b.iter(|| decode(i)));
 }
 
 criterion_group!(benches, criterion_benchmark);
