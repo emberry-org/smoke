6a83f807c6d3ed13c649c8ce0afd70c76ba2375e
devensiv <devensiv@gmail.com>
2023-02-28 23:43:53 +0100

    feat: drain trait for "impl Serialize"

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -2,7 +2,9 @@ mod room_id;
 pub mod signal;
 pub mod emb_message;
 pub mod rhiz_message;
+mod drain;
 
+pub use drain::Drain;
 pub use room_id::RoomId;
 pub use emb_message::EmbMessage;
 pub use emb_message::EMB_MESSAGE_BUF_SIZE;
