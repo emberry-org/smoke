07573534cd8f8d1f7d0278e12095e37b0232ad9b
devensiv <devensiv@gmail.com>
2023-04-14 16:55:53 +0200

    feat: put room request timeout in smoke

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,6 @@ mod user;
 #[cfg(feature = "client")]
 pub use messages::signal::Signal;
 pub use user::User;
+use std::time::Duration;
+
+pub const ROOM_REQ_TIMEOUT: Duration = Duration::from_secs(10);
